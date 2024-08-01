3ff6915e0588fef38879f15ef5caac18622f9e1b
Aaban Saad <aabansaad@gmail.com>
2024-08-01 20:06:02 +0600

    Renamed to Class Compass

--- course-compass/src/App.jsx
+++ course-compass/src/App.jsx
@@ -1,6 +1,8 @@
 import Home from './pages/Home/Home.jsx'
 import { ScrollArea } from '@radix-ui/themes'
 
+import Footer from './components/Footer/Footer.jsx'
+
 function App() {
 
   return (
@@ -8,6 +10,7 @@ function App() {
       <div id='bg'>
         <ScrollArea size="1" type="always" scrollbars="vertical">
           <Home />
+          <Footer />
         </ScrollArea>
       </div>
     </>
