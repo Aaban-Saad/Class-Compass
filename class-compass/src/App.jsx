977048c4761945df992b395f33c882cf4239eaac
Aaban Saad <aabansaad@gmail.com>
2024-08-02 20:38:04 +0600

    trying vercel serverless function

--- class-compass/src/App.jsx
+++ class-compass/src/App.jsx
@@ -6,30 +6,30 @@ import { useState, useEffect } from 'react'
 
 function App() {
 
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // const response = await fetch('/api/');
-        const response = await fetch('https://rds2.northsouth.edu/index.php/common/showofferedcourses');
+        // const response = await fetch('/api/')
+        const response = await fetch('/api/fetch-html')
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error('Network response was not ok')
         }
-        const result = await response.text();
-        setData(result);
+        const result = await response.text()
+        setData(result)
         console.log(result)
       } catch (error) {
-        setError(error);
+        setError(error)
       } finally {
-        setLoading(false);
+        setLoading(false)
       }
     };
 
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
   const [theme, setTheme] = useState("dark")
   const [bgTheme, setBgTheme] = useState("bg-dark")
