0d1cebee1b3f88ee1802782b1f80bf07256bceef
Aaban Saad <aabansaad@gmail.com>
2024-08-03 06:37:36 +0600

    ?

--- class-compass/src/App.jsx
+++ class-compass/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     const fetchData = async () => {
       try {
         // const response = await fetch('/api/')
-        const response = await fetch('/api/fetch-html')
+        const response = await fetch('https://rds2.northsouth.edu/index.php/common/showofferedcourses')
         if (!response.ok) {
           throw new Error('Network response was not ok')
         }
