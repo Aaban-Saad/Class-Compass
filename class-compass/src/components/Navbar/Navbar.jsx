fec66ceb49741efed1d647fffa68020f35f1c0bf
Aaban Saad <aabansaad@gmail.com>
2024-08-03 07:10:28 +0600

    ?

--- class-compass/src/components/Navbar/Navbar.jsx
+++ class-compass/src/components/Navbar/Navbar.jsx
@@ -9,6 +9,7 @@ function Navbar(props) {
 
   return (
     <>
+      test
       <Box style={{ zIndex:999, background: 'var(--indigo-2)', boxShadow: "var(--shadow-3", position: "fixed", width:"100vw", padding: "10px" }}>
         <Container>
           <Button variant="soft" onClick={toggleTheme}>
