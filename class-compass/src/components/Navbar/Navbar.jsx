3205bcbc3656e46cd1d570cca38449a4b4e9ca0c
Aaban Saad <aabansaad@gmail.com>
2024-08-05 11:23:57 +0600

    Finnished Navbar

--- class-compass/src/components/Navbar/Navbar.jsx
+++ class-compass/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,7 @@
-import { Box, Flex, Button, Link } from "@radix-ui/themes"
+import { Box, Flex, Button, Link, DropdownMenu } from "@radix-ui/themes"
 import { Link as RouterLink } from "react-router-dom"
 
 function Navbar(props) {
-
-  // const [homeActive, setHomeActive] = useState()
-  // const [plannerActive, setPlannerActive] = useState()
-  // const [notificationActive, setNotificationActive] = useState()
-
   const toggleTheme = () => {
     props.setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'));
   };
@@ -52,9 +47,39 @@ function Navbar(props) {
               </Button>
             </Link>
             <Box display={{ initial: "block", md: "none" }}>
-              <Button style={{ cursor: "pointer" }} variant="outline">
-                <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.5 3C1.22386 3 1 3.22386 1 3.5C1 3.77614 1.22386 4 1.5 4H13.5C13.7761 4 14 3.77614 14 3.5C14 3.22386 13.7761 3 13.5 3H1.5ZM1 7.5C1 7.22386 1.22386 7 1.5 7H13.5C13.7761 7 14 7.22386 14 7.5C14 7.77614 13.7761 8 13.5 8H1.5C1.22386 8 1 7.77614 1 7.5ZM1 11.5C1 11.2239 1.22386 11 1.5 11H13.5C13.7761 11 14 11.2239 14 11.5C14 11.7761 13.7761 12 13.5 12H1.5C1.22386 12 1 11.7761 1 11.5Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
-              </Button>
+
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Button style={{ cursor: "pointer" }} variant="outline">
+                    <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.5 3C1.22386 3 1 3.22386 1 3.5C1 3.77614 1.22386 4 1.5 4H13.5C13.7761 4 14 3.77614 14 3.5C14 3.22386 13.7761 3 13.5 3H1.5ZM1 7.5C1 7.22386 1.22386 7 1.5 7H13.5C13.7761 7 14 7.22386 14 7.5C14 7.77614 13.7761 8 13.5 8H1.5C1.22386 8 1 7.77614 1 7.5ZM1 11.5C1 11.2239 1.22386 11 1.5 11H13.5C13.7761 11 14 11.2239 14 11.5C14 11.7761 13.7761 12 13.5 12H1.5C1.22386 12 1 11.7761 1 11.5Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+                  </Button>
+                </DropdownMenu.Trigger>
+
+                <DropdownMenu.Content variant="soft" color="indigo">
+                  <RouterLink to="/" style={{ textDecoration: "none", color: "var(--gray-12)" }}>
+                    <DropdownMenu.Item style={props.page === "home" ? { backgroundColor: "var(--indigo-3)" } : {}}>
+                      Home
+                    </DropdownMenu.Item>
+                  </RouterLink>
+
+                  <RouterLink to="/planner" style={{ textDecoration: "none", color: "var(--gray-12)" }}>
+                    <DropdownMenu.Item style={props.page === "planner" ? { backgroundColor: "var(--indigo-3)" } : {}}>
+                      Auto Planner
+                    </DropdownMenu.Item>
+                  </RouterLink>
+
+                  <RouterLink to="/notification" style={{ textDecoration: "none", color: "var(--gray-12)" }}>
+                    <DropdownMenu.Item style={props.page === "notification" ? { backgroundColor: "var(--indigo-3)" } : {}}>
+                      Notification
+                    </DropdownMenu.Item>
+                  </RouterLink>
+
+                  <DropdownMenu.Separator />
+
+                  <DropdownMenu.Item >My Compass</DropdownMenu.Item>
+                  
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
             </Box>
           </Flex>
         </Flex>
