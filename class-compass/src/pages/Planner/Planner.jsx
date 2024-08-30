407c0a4c0faf8726bb4e15ccda2c529bc9ba7890
Aaban Saad <aabansaad@gmail.com>
2024-08-30 20:16:59 +0600

    Planner course add with different name and some bug fixes

--- class-compass/src/pages/Planner/Planner.jsx
+++ class-compass/src/pages/Planner/Planner.jsx
@@ -1,6 +1,7 @@
-import { Heading, Card, Flex, TextField, Button, Badge, Grid, Select, Checkbox, CheckboxCards, IconButton } from '@radix-ui/themes'
+import { Heading, Text, Card, Flex, TextField, Button, Badge, Grid, Select, Checkbox, CheckboxCards, IconButton } from '@radix-ui/themes'
 import Navbar from '../../components/Navbar/Navbar'
 import { useEffect, useState } from 'react'
+import { paddingPropDefs } from '@radix-ui/themes/dist/cjs/props/padding.props'
 
 function Planner(props) {
   const [data, setData] = useState(null)
@@ -22,7 +23,6 @@ function Planner(props) {
   const [mwPossible, setMwPossible] = useState(true)
   const [raPossible, setRaPossible] = useState(true)
   const [fPossible, setFPossible] = useState(true)
-  const [preferredClassDays, setPreferredDays] = useState(4)
   const [maxClassesPerDay, setMaxClassesPerDay] = useState(4)
   const [maxBtoBClasses, setMaxBtoBClasses] = useState(4)
   const [avoidLongGaps, setAvoidLongGaps] = useState(false)
@@ -142,12 +142,30 @@ function Planner(props) {
     return {}
   }
 
+  function findCourseBySubstring(searchString) {
+    searchString = searchString.toLowerCase();
+
+    for (const day in dayCombinations) {
+      const courses = dayCombinations[day];
+      for (const courseObj of courses) {
+        if (courseObj.course.toLowerCase().includes(searchString)) {
+          return courseObj.course; // Return the course name if a match is found
+        }
+      }
+    }
+    return ""; // Return null if no match is found
+  }
+
 
   const addCourse = (course) => {
-    if (course !== "" && !courses.hasOwnProperty(course)) {
-      setCourses((prev) => ({ ...prev, [course.toUpperCase()]: { taken: false, time: "none", sections: [] } }))
+    let c = findCourseBySubstring(course)
+    if (c !== "" && !courses.hasOwnProperty(c)) {
+      setCourses((prev) => ({ ...prev, [c.toUpperCase()]: { taken: false, time: "none", sections: [] } }))
+      setCourse(() => "")
+      document.getElementById("planner-course-input").value = ""
     }
   }
+
 
   const removeCourse = (courseToRemove) => {
     setCourses((prev) => {
@@ -235,15 +253,15 @@ function Planner(props) {
     }
   }
 
-  function isTimeTaken(day, begTime) {
+  function isTimeTaken(day, time) {
     day = day.toUpperCase()
     for (let c in courses) {
       let timeInMin = timeToMinutes(courses[c].time)
       try {
         if (((timeInMin.days).includes(day) ||
           day.includes(timeInMin.days)) &&
-          timeInMin.startTime <= begTime &&
-          timeInMin.endTime >= begTime) {
+          timeInMin.startTime <= time &&
+          timeInMin.endTime >= time) {
           return true
         }
       } catch (error) {
@@ -254,17 +272,16 @@ function Planner(props) {
     return false;
   }
 
-  function isTimeGap(day, begTime) {
+  function isTimeGap(day, time) {
     for (let gap of gaps) {
       let timeInMin = timeToMinutes(gap);
-      console.log(gap, timeInMin, begTime, timeInMin.startTime <= begTime && timeInMin.endTime >= begTime);
       try {
         if (
           (timeInMin.days === "ALL-DAYS" ||
             timeInMin.days.includes(day) ||
             day.includes(timeInMin.days)) &&
-          timeInMin.startTime <= begTime &&
-          timeInMin.endTime >= begTime
+          timeInMin.startTime <= time &&
+          timeInMin.endTime >= time
         ) {
           return true;
         }
@@ -331,27 +348,27 @@ function Planner(props) {
           }
 
           // If maxed back to back classes then move to next slot
-          if(bToBClasses >= maxBtoBClasses) {
+          if (bToBClasses >= maxBtoBClasses) {
             bToBClasses = 0 // Reset and move to next slot
             continue
           }
 
           // Keeping long gap: if half or, more classes are taken already then skip a few slots
           console.log(keepLongGaps, avoidLongGaps, classesInADay[day], maxClassesPerDay)
-          if(!keptLongGap && keepLongGaps && classesInADay[day] === Math.floor(maxClassesPerDay / 2)) {
+          if (!keptLongGap && keepLongGaps && classesInADay[day] === Math.floor(maxClassesPerDay / 2)) {
             j += 2
-            console.log("1____________",classesInADay[day] , Math.floor(Object.keys(courses).length / 2))
+            console.log("1____________", classesInADay[day], Math.floor(Object.keys(courses).length / 2))
             keptLongGap = true
             continue
-          } else if(!keptLongGap && keepLongGaps && classesInADay[day] === Math.floor(Object.keys(courses).length / 2)) {
+          } else if (!keptLongGap && keepLongGaps && classesInADay[day] === Math.floor(Object.keys(courses).length / 2)) {
             j += 2
-            console.log("2____________",classesInADay[day] , Math.floor(Object.keys(courses).length / 2))
+            console.log("2____________", classesInADay[day], Math.floor(Object.keys(courses).length / 2))
             keptLongGap = true
             continue
           }
 
           // Avoid prayer time: 12:15 - 1:30 and after 4:20
-          if(timeSlots[j] === 735 || timeSlots[j] >= 990) {
+          if (timeSlots[j] === 735 || timeSlots[j] >= 990) {
             continue
           }
 
@@ -363,9 +380,16 @@ function Planner(props) {
             }
             else if ('course' in courseObj) {
               console.log("updated ", courseObj)
-              day = timeToMinutes(courseObj.time).days // Updating day with the found day(s)
-              if (isTimeTaken(day, timeSlots[j]) && !isTimeGap(day, timeSlots[j])) {
+              let timeInMin = timeToMinutes(courseObj.time)
+              day = timeInMin.days // Updating day with the found day(s)
+              let endTime = timeInMin.endTime
+              if (isTimeTaken(day, timeSlots[j]) || isTimeGap(day, timeSlots[j])) {
                 // Second check with the updated day(s)
+                bToBClasses = 0 // Reset back to back classes
+                continue
+              }
+              else if(isTimeTaken(day, endTime) || isTimeGap(day, endTime)) {
+                // Second check if the end time is available
                 bToBClasses = 0 // Reset back to back classes
                 continue
               }
@@ -380,7 +404,7 @@ function Planner(props) {
                 bToBClasses++
                 // Keeping track of the number of classes per day
                 classesInADay[day.charAt(0)]++
-                if(day.length > 1) classesInADay[day.charAt(1)]++
+                if (day.length > 1) classesInADay[day.charAt(1)]++
                 console.log(classesInADay)
 
                 break
@@ -412,7 +436,7 @@ function Planner(props) {
                     Add Courses
                   </Heading>
                   <Flex gap="2" justify="center" align="center">
-                    <TextField.Root size="2" onChange={(event) => setCourse(event.target.value)} placeholder="Course Name" />
+                    <TextField.Root id='planner-course-input' size="2" onChange={(event) => setCourse(event.target.value)} placeholder="Course Name" />
                     <Button color="green" onClick={() => addCourse(course)}>
                       Add
                     </Button>
@@ -423,14 +447,17 @@ function Planner(props) {
 
                       {Object.keys(courses).map((courseKey) => {
                         return (
-                          <Badge key={courseKey} variant='surface' size="3" color="iris">
-                            <span style={{ maxWidth: "70px", overflow: 'hidden' }}>
-                              {courseKey}
-                            </span>
-                            <Button onClick={() => removeCourse(courseKey)} color='red' size="1" variant='surface'>
-                              <svg width="10" height="10" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.8536 2.85355C13.0488 2.65829 13.0488 2.34171 12.8536 2.14645C12.6583 1.95118 12.3417 1.95118 12.1464 2.14645L7.5 6.79289L2.85355 2.14645C2.65829 1.95118 2.34171 1.95118 2.14645 2.14645C1.95118 2.34171 1.95118 2.65829 2.14645 2.85355L6.79289 7.5L2.14645 12.1464C1.95118 12.3417 1.95118 12.6583 2.14645 12.8536C2.34171 13.0488 2.65829 13.0488 2.85355 12.8536L7.5 8.20711L12.1464 12.8536C12.3417 13.0488 12.6583 13.0488 12.8536 12.8536C13.0488 12.6583 13.0488 12.3417 12.8536 12.1464L8.20711 7.5L12.8536 2.85355Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
-                            </Button>
-                          </Badge>
+                          <Card key={courseKey} variant='classic' style={{backgroundColor:"var(--indigo-2)", border:"2px solid var(--indigo-7)"}}>
+                            <Flex justify="center" align="center" gap="3" color="red" >
+                              <Text size="2" color='indigo' style={{ maxWidth: "70px" }}>
+                                {courseKey}
+                              </Text>
+
+                              <Button onClick={() => removeCourse(courseKey)} color='red' size="1" variant='surface'>
+                                <svg width="10" height="10" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.8536 2.85355C13.0488 2.65829 13.0488 2.34171 12.8536 2.14645C12.6583 1.95118 12.3417 1.95118 12.1464 2.14645L7.5 6.79289L2.85355 2.14645C2.65829 1.95118 2.34171 1.95118 2.14645 2.14645C1.95118 2.34171 1.95118 2.65829 2.14645 2.85355L6.79289 7.5L2.14645 12.1464C1.95118 12.3417 1.95118 12.6583 2.14645 12.8536C2.34171 13.0488 2.65829 13.0488 2.85355 12.8536L7.5 8.20711L12.1464 12.8536C12.3417 13.0488 12.6583 13.0488 12.8536 12.8536C13.0488 12.6583 13.0488 12.3417 12.8536 12.1464L8.20711 7.5L12.8536 2.85355Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
+                              </Button>
+                            </Flex>
+                          </Card>
                         );
                       })}
                     </Grid>
@@ -523,33 +550,6 @@ function Planner(props) {
                         <IconButton variant='soft'>
                           <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.49991 0.876892C3.84222 0.876892 0.877075 3.84204 0.877075 7.49972C0.877075 11.1574 3.84222 14.1226 7.49991 14.1226C11.1576 14.1226 14.1227 11.1574 14.1227 7.49972C14.1227 3.84204 11.1576 0.876892 7.49991 0.876892ZM1.82707 7.49972C1.82707 4.36671 4.36689 1.82689 7.49991 1.82689C10.6329 1.82689 13.1727 4.36671 13.1727 7.49972C13.1727 10.6327 10.6329 13.1726 7.49991 13.1726C4.36689 13.1726 1.82707 10.6327 1.82707 7.49972ZM8.24992 4.49999C8.24992 4.9142 7.91413 5.24999 7.49992 5.24999C7.08571 5.24999 6.74992 4.9142 6.74992 4.49999C6.74992 4.08577 7.08571 3.74999 7.49992 3.74999C7.91413 3.74999 8.24992 4.08577 8.24992 4.49999ZM6.00003 5.99999H6.50003H7.50003C7.77618 5.99999 8.00003 6.22384 8.00003 6.49999V9.99999H8.50003H9.00003V11H8.50003H7.50003H6.50003H6.00003V9.99999H6.50003H7.00003V6.99999H6.50003H6.00003V5.99999Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
                         </IconButton>
-                        Preferred class days:
-                      </Flex>
-
-                      <Flex justify="center" align="center" gap="2">
-                        <Button size="1" variant='soft' onClick={() => {
-                          setPreferredDays((prev) => prev - 1)
-                          if (preferredClassDays <= 1) setPreferredDays(() => 1)
-                        }}>
-                          <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2.25 7.5C2.25 7.22386 2.47386 7 2.75 7H12.25C12.5261 7 12.75 7.22386 12.75 7.5C12.75 7.77614 12.5261 8 12.25 8H2.75C2.47386 8 2.25 7.77614 2.25 7.5Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
-                        </Button>
-                        <span>{preferredClassDays}</span>
-                        <Button size="1" variant='soft' onClick={() => {
-                          setPreferredDays((prev) => prev + 1)
-                          if (preferredClassDays >= 7) setPreferredDays(() => 7)
-                        }}>
-                          <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 2.75C8 2.47386 7.77614 2.25 7.5 2.25C7.22386 2.25 7 2.47386 7 2.75V7H2.75C2.47386 7 2.25 7.22386 2.25 7.5C2.25 7.77614 2.47386 8 2.75 8H7V12.25C7 12.5261 7.22386 12.75 7.5 12.75C7.77614 12.75 8 12.5261 8 12.25V8H12.25C12.5261 8 12.75 7.77614 12.75 7.5C12.75 7.22386 12.5261 7 12.25 7H8V2.75Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
-                        </Button>
-                      </Flex>
-                    </Flex>
-                  </Card>
-
-                  <Card style={{ width: "100%" }}>
-                    <Flex justify="between" align="center" gap="2">
-                      <Flex justify="center" align="center" gap="1" style={{ textAlign: "left" }}>
-                        <IconButton variant='soft'>
-                          <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.49991 0.876892C3.84222 0.876892 0.877075 3.84204 0.877075 7.49972C0.877075 11.1574 3.84222 14.1226 7.49991 14.1226C11.1576 14.1226 14.1227 11.1574 14.1227 7.49972C14.1227 3.84204 11.1576 0.876892 7.49991 0.876892ZM1.82707 7.49972C1.82707 4.36671 4.36689 1.82689 7.49991 1.82689C10.6329 1.82689 13.1727 4.36671 13.1727 7.49972C13.1727 10.6327 10.6329 13.1726 7.49991 13.1726C4.36689 13.1726 1.82707 10.6327 1.82707 7.49972ZM8.24992 4.49999C8.24992 4.9142 7.91413 5.24999 7.49992 5.24999C7.08571 5.24999 6.74992 4.9142 6.74992 4.49999C6.74992 4.08577 7.08571 3.74999 7.49992 3.74999C7.91413 3.74999 8.24992 4.08577 8.24992 4.49999ZM6.00003 5.99999H6.50003H7.50003C7.77618 5.99999 8.00003 6.22384 8.00003 6.49999V9.99999H8.50003H9.00003V11H8.50003H7.50003H6.50003H6.00003V9.99999H6.50003H7.00003V6.99999H6.50003H6.00003V5.99999Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
-                        </IconButton>
                         Maximum number of classes per day
                       </Flex>
 
@@ -609,11 +609,11 @@ function Planner(props) {
 
                       <Checkbox id="avoid-long-gap" onClick={() => setAvoidLongGaps((prev) => {
                         const current = !prev
-                        if(keepLongGaps) {
+                        if (keepLongGaps) {
                           document.getElementById('keep-long-gap').click()
                         }
                         return current
-                        })} />
+                      })} />
                     </Flex>
                   </Card>
 
@@ -628,11 +628,11 @@ function Planner(props) {
 
                       <Checkbox id="keep-long-gap" onClick={() => setKeepLongGaps((prev) => {
                         const current = !prev
-                        if(avoidLongGaps) {
+                        if (avoidLongGaps) {
                           document.getElementById('avoid-long-gap').click()
                         }
                         return current
-                        })} 
+                      })}
                       />
                     </Flex>
                   </Card>
